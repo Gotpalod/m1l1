eacfa423a0fccf862c82e2891671780d1a874da5
Gotpalod <145790842+Gotpalod@users.noreply.github.com>
2023-10-13 19:14:47 +0300

    Update main.py m1l4

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "РОФЛ": "ШУТКА",
-            "ПОН": "ПОНЯТНО"
-            
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("токого словечка нету")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def Minecraft(ctx):
+    await ctx.send(f'Майнкрафт это мая жизнь МАЙНКРАФФФФФФФФФТ')
+
+@bot.command()
+async def pon(ctx, count_heh = 5):
+    await ctx.send("ПОН" * count_heh)
+
+bot.run("MTE1OTg3NzgzNjcxOTUzODIyNg.GBOUUX.ojW5CJzRLpUyw-2phaL8ymS8dLIEmT2ecQSCJs")
+
